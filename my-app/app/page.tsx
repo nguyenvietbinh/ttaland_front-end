2d499144234dc4c145c440402c006ae0715471ec
nguyenvietbinh <nguyenvietbinh006@gmail.com>
2025-04-24 08:44:01 +0700

    cloned _topbar

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,35 +1,12 @@
 
 'use client';
 
-import { NextResponse } from 'next/server';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-
+import _topbar from '../components/partials/_topbar'
 const Home = () => {
-  const [message, setMessage] = useState<string>('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/api/');
-        const res = NextResponse.json(response.data);
-        const data = await res.json();
-        setMessage(data.message);
-      } catch (error) {
-        console.error('Error fetching data from API:', error);
-        setMessage('Error fetching data');
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1 className="text-gray-600">{message || 'Loading...'}</h1>
-      <div className='text-white'>
-        blbal
-      </div>
+      <_topbar/>
     </div>
   );
 };
---
+++ my-app/components/partials/_topbar.tsx
@@ -0,0 +1,46 @@
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelopeOpen, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram, faLinkedin, faSquarePinterest, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+const _topbar = () => {
+  return (
+    <div>
+      <section id="top-bar" className="p-6 bg-gray-200 text-center">
+              <div className="flex flex-wrap items-center text-gray-700">
+                  <div className="w-full md:w-1/3 mb-2 md:mb-0">
+                    <FontAwesomeIcon icon={faPhone} className=""></FontAwesomeIcon> +91 1234567890
+                  </div>
+                  
+                  <div className="w-full md:w-1/3 mb-2 md:mb-0">
+                    <FontAwesomeIcon icon={faEnvelopeOpen} className="fas fa-envelope-open mr-2"></FontAwesomeIcon> contact@realestate.com
+                  </div>
+                  <div className="w-full md:w-1/3">
+                      <div className="flex justify-center space-x-4">
+                          <a href="#" className="text-gray-700 hover:text-blue-400">
+                              <FontAwesomeIcon icon={faTwitter} className="fab fa-twitter"></FontAwesomeIcon>
+                          </a>
+                          <a href="#" className="text-gray-700 hover:text-blue-600">
+                              <FontAwesomeIcon icon={faFacebook} className="fab fa-facebook"></FontAwesomeIcon>
+                          </a>
+                          <a href="#" className="text-gray-700 hover:text-blue-700">
+                              <FontAwesomeIcon icon={faLinkedin} className="fab fa-linkedin"></FontAwesomeIcon>
+                          </a>
+                          <a href="#" className="text-gray-700 hover:text-pink-600">
+                              <FontAwesomeIcon icon={faInstagram} className="fab fa-instagram"></FontAwesomeIcon>
+                          </a>
+                          <a href="#" className="text-gray-700 hover:text-red-600">
+                              <FontAwesomeIcon icon={faSquarePinterest} className="fab fa-pinterest"></FontAwesomeIcon>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+      </section>
+    </div>
+  )
+}
+
+export default _topbar
+
+
