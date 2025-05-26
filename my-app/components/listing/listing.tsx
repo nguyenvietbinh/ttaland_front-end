4524878bef1b7e084073a7b783d88f510272f4ae
nguyenvietbinh <nguyenvietbinh006@gmail.com>
2025-05-26 09:46:30 +0700

    change bgimg, update sub_navbar

--- my-app/components/listing/listing.tsx
+++ my-app/components/listing/listing.tsx
@@ -23,52 +23,54 @@ const Listing = () => {
     title = 'Dự Án'
   }
   return (
-    <div className="w-full xl:w-[1280px] 2xl:w-[1536px] h-auto mx-auto mt-10 text-white">
-    <div className='text-4xl md:text-5xl text-center text-white my-8'>{title} Nổi Bật</div>
-    <_sub_avbar/>
-      <div className={category.includes(list_path[2]) ? "h-auto mx-4 md:bg-black/30 bg-none px-2 xl:mx-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8" : "hidden"}>
-                {items.map((item) => (
-                  <div key={item} className=" bg-gray-600 rounded-sm text-center">
-                    <div className='absolute'>  
-                      <div className='m-4 py-1 px-3 inline-block bg-gray-700 text-white rounded-sm opacity-90'>800.000.000 VND</div>
-                    </div>
-                    <div className=''>
-                    <img src='/img/showcase.png' alt="" className="min-w-full"/>
-                      <div className="w-full h-auto border-t-[1px] border-gray-500 border-solid px-2">
-                        <p className="text-2xl my-2">Property Name</p>
-                        <span>
-                          <FontAwesomeIcon icon={faMapMarker} className="mr-1"></FontAwesomeIcon>
-                        </span>
-                        <span>Location, loc</span>
-                      </div>
-                      <div className='w-full h-auto border-t-[1px] border-gray-500 border-solid grid grid-cols-2 text-left py-4 px-2'>
-                        <div>
-                          <FontAwesomeIcon icon={faThLarge} className='mr-1'></FontAwesomeIcon> Sqft: 1000
-                        </div>
-                        <div>
-                          <FontAwesomeIcon icon={faCar}></FontAwesomeIcon> Garage: 2
-                        </div>
-                        <div>
-                          <FontAwesomeIcon icon={faBed}></FontAwesomeIcon> Bedrooms: {item}
-                        </div>
-                        <div>
-                          <FontAwesomeIcon icon={faBath}></FontAwesomeIcon> Bathroom: 2
-                        </div>
-                      </div>
-                      <div className='w-full h-auto border-t-[1px] border-gray-500 border-solid text-left py-4 px-2'>
-                        <div>
-                          <FontAwesomeIcon icon={faUser}></FontAwesomeIcon> Nguyen Viet Binh
-                        </div>
-                        <div>
-                          <FontAwesomeIcon icon={faClock}></FontAwesomeIcon> 1 year
-                        </div>
-                      </div>
-                      <div className='w-full h-auto border-t-[1px] border-gray-500 border-solid text-left py-8 px-2'>
-                        <Link href={`${path_name}/show/displayer`}><div className='btn bg-blue-950 text-white w-full'>More Infor</div></Link>
-                      </div>
-                    </div>
+    <div className=''>
+      <div className="w-full xl:w-[1280px] 2xl:w-[1536px] h-auto mx-auto mt-10 text-white">
+        <div className='text-4xl md:text-5xl text-center text-white my-8'>{title} Nổi Bật</div>
+        <_sub_avbar/>
+        <div className={category.includes(list_path[2]) ? "h-auto mx-4 bg-none px-2 xl:mx-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8" : "hidden"}>
+          {items.map((item) => (
+            <div key={item} className=" bg-gray-600 rounded-sm text-center">
+              <div className='absolute'>  
+                <div className='m-4 py-1 px-3 inline-block bg-gray-700 text-white rounded-sm opacity-90'>800.000.000 VND</div>
+              </div>
+              <div className=''>
+              <img src='/img/showcase.png' alt="" className="min-w-full"/>
+                <div className="w-full h-auto border-t-[1px] border-gray-500 border-solid px-2">
+                  <p className="text-2xl my-2">Property Name</p>
+                  <span>
+                    <FontAwesomeIcon icon={faMapMarker} className="mr-1"></FontAwesomeIcon>
+                  </span>
+                  <span>Location, loc</span>
+                </div>
+                <div className='w-full h-auto border-t-[1px] border-gray-500 border-solid grid grid-cols-2 text-left py-4 px-2'>
+                  <div>
+                    <FontAwesomeIcon icon={faThLarge} className='mr-1'></FontAwesomeIcon> Sqft: 1000
                   </div>
-                ))}
+                  <div>
+                    <FontAwesomeIcon icon={faCar}></FontAwesomeIcon> Garage: 2
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faBed}></FontAwesomeIcon> Bedrooms: {item}
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faBath}></FontAwesomeIcon> Bathroom: 2
+                  </div>
+                </div>
+                <div className='w-full h-auto border-t-[1px] border-gray-500 border-solid text-left py-4 px-2'>
+                  <div>
+                    <FontAwesomeIcon icon={faUser}></FontAwesomeIcon> Nguyen Viet Binh
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faClock}></FontAwesomeIcon> 1 year
+                  </div>
+                </div>
+                <div className='w-full h-auto border-t-[1px] border-gray-500 border-solid text-left py-8 px-2'>
+                  <Link href={`${path_name}/show/displayer`}><div className='btn bg-blue-950 text-white w-full'>More Infor</div></Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
--- my-app/components/listing/sub_navbar.tsx
+++ my-app/components/listing/sub_navbar.tsx
@@ -8,13 +8,12 @@ const SubNavbar = () => {
   
   return (
     <div>
-      <div className="hidden h-auto md:flex mx-4 xl:mx-0 justify-between text-3xl">
-        <Link className={list_path[2] === 'dat_nen' ? "w-full text-center border-solid border-gray-400 border-t-2 border-x-2 py-2 bg-black/30 rounded-t-sm" : "w-full text-gray-200 text-center py-2 border-solid border-gray-400 border-b-2"} href={`/${list_path[1]}/dat_nen`}>Đất Nền</Link>
-        <Link className={list_path[2] === 'nha_pho' ? "w-full text-center border-solid border-gray-400 border-t-2 border-x-2 py-2 bg-black/30 rounded-t-sm" : "w-full text-gray-200 text-center py-2 border-solid border-gray-400 border-b-2"} href={`/${list_path[1]}/nha_pho`}>Nhà Phố</Link>
-        <Link className={list_path[2] === 'biet_thu' ? "w-full text-center border-solid border-gray-400 border-t-2 border-x-2 py-2 bg-black/30 rounded-t-sm" : "w-full text-gray-200 text-center py-2 border-solid border-gray-400 border-b-2"} href={`/${list_path[1]}/biet_thu`}>Biệt Thự</Link>
-        <Link className={list_path[2] === 'can_ho' ? "w-full text-center border-solid border-gray-400 border-t-2 border-x-2 py-2 bg-black/30 rounded-t-sm" : "w-full text-gray-200 text-center py-2 border-solid border-gray-400 border-b-2"} href={`/${list_path[1]}/can_ho`}>Căn Hộ</Link>
+      <div className="hidden h-auto md:flex mx-4 mb-4 xl:mx-0 justify-between text-3xl">
+        <Link className={list_path[2] === 'dat_nen' ? "w-full text-center border-solid border-gray-300 border-t-2 border-r-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/dat_nen`}>Đất Nền</Link>
+        <Link className={list_path[2] === 'nha_pho' ? "w-full text-center border-solid border-gray-300 border-t-2 border-x-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/nha_pho`}>Nhà Phố</Link>
+        <Link className={list_path[2] === 'biet_thu' ? "w-full text-center border-solid border-gray-300 border-t-2 border-x-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/biet_thu`}>Biệt Thự</Link>
+        <Link className={list_path[2] === 'can_ho' ? "w-full text-center border-solid border-gray-300 border-t-2 border-l-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/can_ho`}>Căn Hộ</Link>
       </div>
-      <div className="h-4 hidden md:block mx-4 xl:mx-0 bg-black/30"></div>
     </div>
     
   )
