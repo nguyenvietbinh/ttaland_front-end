67eb8791e30a3887b023ab39b06c8ac3ab87dab8
nguyenvietbinh <nguyenvietbinh006@gmail.com>
2025-05-29 10:36:24 +0700

    add loading page

---
+++ my-app/app/loading/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+
+const Loading = () => {
+  return (
+    <div className="fixed  inset-0 bg-gray-400 z-50 flex flex-col items-center justify-center">
+      <div>
+      <span className="mt-4 text-black text-3xl">Đang tải trang, vui lòng chờ</span>
+      <span className="loading mx-1 loading-dots loading-sm text-black"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
