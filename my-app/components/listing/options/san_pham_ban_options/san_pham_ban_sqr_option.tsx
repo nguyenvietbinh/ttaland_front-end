2e5b771baf9cc60a34783b28e7ed86a1f6414d58
nguyenvietbinh <nguyenvietbinh006@gmail.com>
2025-07-09 15:34:31 +0700

    add price_option and sqr_option logic

---
+++ my-app/components/listing/options/san_pham_ban_options/san_pham_ban_sqr_option.tsx
@@ -0,0 +1,118 @@
+'use client'
+
+import { usePathname, useSearchParams } from "next/navigation"
+import { useRouter } from 'next/navigation';
+import { useState, useEffect, useRef } from "react"
+
+const San_pham_ban_sqr_option = () => {
+  const router = useRouter()
+  const path_name = usePathname()
+  const list_path = path_name.split('/')
+  const searchParams = useSearchParams()
+  const [showOptions, setShowOptions] = useState<boolean>(false)
+  const priceTagName = (searchParams.get('sqr_min') && searchParams.get('sqr_max')) ? `${searchParams.get('sqr_min')} - ${searchParams.get('sqr_max')}` : 'Diện Tích'
+  const optionContainerRef = useRef<HTMLDivElement>(null);
+  const tagRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        tagRef.current && 
+        !tagRef.current.contains(event.target as Node) &&
+        optionContainerRef.current && 
+        !optionContainerRef.current.contains(event.target as Node)
+      ) {
+        setShowOptions(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const moveTo = (sqr_min: string, sqr_max: string) => {
+      const price_min = searchParams.get('price_min')
+      const price_max = searchParams.get('price_max')
+      const locations = searchParams.get('locations')
+      const bed_room_min = searchParams.get('bed_room_min')
+      const bed_room_max = searchParams.get('bed_room_max')
+      const bath_room_min = searchParams.get('bath_room_min')
+      const bath_room_max = searchParams.get('bath_room_max')
+
+      const queryParams: Record<string, string> = {};
+
+      queryParams.sqr_min = sqr_min
+      queryParams.sqr_max = sqr_max
+      if (price_min) queryParams.price_min = price_min
+      if (price_max) queryParams.price_max = price_max
+      if (locations) queryParams.locations = locations
+      if (bed_room_min) queryParams.bed_room_min = bed_room_min;
+      if (bed_room_max) queryParams.bed_room_max = bed_room_max;
+      if (bath_room_min) queryParams.bath_room_min = bath_room_min;
+      if (bath_room_max) queryParams.bath_room_max = bath_room_max;
+
+      const queryString = new URLSearchParams(queryParams).toString();
+      router.push(`/${list_path[1]}/${list_path[2]}?${queryString}`);
+  }
+
+  const handleOptionsClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const spanElement = e.currentTarget.querySelector('span');
+    
+    if (spanElement) {
+      const spanContent = spanElement.textContent;
+      if (spanContent === 'Tất Cả' || spanContent === null) {
+        moveTo('Từ: 0 m²', 'Đến: 500 m²')
+      } else {
+        moveTo(spanContent.split(' - ')[0], spanContent.split(' - ')[1])
+      }
+
+    }
+    setShowOptions(false)
+    
+  };
+
+  return (
+    <div>
+      <div ref={tagRef} onClick={() => {setShowOptions(preVal => !preVal)}} className="border-[1px] border-gray-400 p-1 px-4 flex gap-2 cursor-pointer hover:border-white rounded-sm">
+        <p className=" overflow-auto text-nowrap">{ priceTagName }</p>
+        <div className="flex items-center">
+          <img src="/img/icons/arrow.png" alt="" className="h-2"/>
+        </div>
+      </div>
+      {showOptions && (
+        <div ref={optionContainerRef} className="absolute text-nowrap mt-2 z-49 backdrop-blur-md bg-black/60  text-white inline-block p-4 rounded-box max-h-80 overflow-auto shadow-lg">
+          <div className="gap-2 flex-col space-y-2">
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Tất Cả</span>
+            </div>
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Từ: 0 m² - Đến: 30 m²</span>
+            </div>
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Từ: 30 m² - Đến: 50 m²</span>
+            </div>
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Từ: 50 m² - Đến: 100 m²</span>
+            </div>
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Từ: 100 m² - Đến: 150 m²</span>
+            </div>
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Từ: 200 m² - Đến: 250 m²</span>
+            </div>
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Từ: 300 m² - Đến: 350 m²</span>
+            </div>
+            <div onClick={handleOptionsClick} className="flex gap-8 justify-between cursor-pointer hover:bg-white/10 py-2 px-4 rounded-md">
+              <span>Từ: 400 m² - Đến: 500 m²</span>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default San_pham_ban_sqr_option
--- my-app/components/listing/sub_navbar.tsx
+++ my-app/components/listing/sub_navbar.tsx
@@ -1,4 +1,11 @@
 'use client'
+
+import San_pham_ban_price_option from "./options/san_pham_ban_options/san_pham_ban_price_option"
+import San_pham_ban_sqr_option from "./options/san_pham_ban_options/san_pham_ban_sqr_option"
+import San_pham_cho_thue_price_option from "./options/san_pham_cho_thue_options/san_pham_cho_thue_price_option"
+import San_pham_cho_thue_sqr_option from "./options/san_pham_cho_thue_options/san_pham_cho_thue_sqr_option"
+import Du_an_price_option from "./options/du_an_options/du_an_price_option"
+import Du_an_sqr_option from "./options/du_an_options/du_an_sqr_option"
 import Link from "next/link"
 import { usePathname, useSearchParams } from "next/navigation"
 
@@ -7,8 +14,7 @@ const SubNavbar = () => {
   const path_name = usePathname()
   const searchParams = useSearchParams()
   const list_path = path_name.split('/')
-  const priceTagName = (searchParams.get('price_min') && searchParams.get('price_max')) ? `${searchParams.get('price_min')} - ${searchParams.get('price_max')}` : 'Mức Giá'
-  const sqrTagName = (searchParams.get('sqr_min') && searchParams.get('sqr_max')) ? `${searchParams.get('sqr_min')} - ${searchParams.get('sqr_max')}` : 'Diện Tích'
+
   return (
     <div className="flex-col mt-1 mb-4">
       <div className="hidden h-auto md:flex mx-4 xl:mx-0 justify-between text-3xl">
@@ -18,19 +24,10 @@ const SubNavbar = () => {
         <Link className={(list_path[2] === 'can_ho') ? "w-full text-center border-solid border-gray-300 border-t-2 border-l-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/can_ho?${searchParams.toString()}`}>Căn Hộ</Link>
       </div>
       <div className="md:flex items-center inline-block space-y-2 md:space-y-0 py-2 gap-4">
-        <div className="border-[1px] border-gray-400 p-1 px-4 flex gap-2 cursor-pointer hover:border-white rounded-sm">
-          <p className=" overflow-auto text-nowrap">{ priceTagName }</p>
-          <div className="flex items-center">
-            <img src="/img/icons/arrow.png" alt="" className="h-2"/>
-          </div>
-        </div>
-        <div className="border-[1px] border-gray-400 p-1 px-4 flex gap-2 cursor-pointer hover:border-white rounded-sm">
-          <p className=" overflow-auto text-nowrap">{ sqrTagName }</p>
-          <div className="flex items-center">
-            <img src="/img/icons/arrow.png" alt="" className="h-2"/>
-          </div>
-        </div>
+        {(list_path[1] === 'san_pham_ban') ? (<San_pham_ban_price_option />) : (list_path[1] === 'san_pham_cho_thue') ? (<San_pham_cho_thue_price_option />) : (<Du_an_price_option />)}
+        {(list_path[1] === 'san_pham_ban') ? (<San_pham_ban_sqr_option />) : (list_path[1] === 'san_pham_cho_thue') ? (<San_pham_cho_thue_sqr_option />) : (<Du_an_sqr_option />)}
       </div>
+
     </div>
     
   )
