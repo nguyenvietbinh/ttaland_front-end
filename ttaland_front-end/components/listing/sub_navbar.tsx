5e5f42d38bc840c33e222f93c95a2577bfd5ab0f
nguyenvietbinh <nguyenvietbinh006@gmail.com>
2025-08-29 09:49:04 +0700

    add line height

--- ttaland_front-end/components/listing/sub_navbar.tsx
+++ ttaland_front-end/components/listing/sub_navbar.tsx
@@ -25,17 +25,17 @@ const Sub_navbar = ({currentPropertyType, isForSale, isForRent}: Sub_navbar_prop
   }
   return (
     <div className={`flex-col sticky z-40 rounded-b-lg transition-all duration-500 top-0 mb-2 mt-8 px-2`}>
-      <div className={`text-4xl flex transition-all duration-100 text-nowrap`}>
+      <div className={`text-3xl flex transition-all duration-100 text-nowrap`}>
         <p className='hidden md:flex'>Bất động sản tại:&ensp;</p>
-        <div className='flex overflow-auto'>{locations?.map((loc, index) => (
+        <div className='flex'>{locations?.map((loc, index) => (
             <p key={index} className='cursor-pointer'>{loc}{(index === locations.length - 1) ? '' : ','}&ensp;</p>
         ))}</div>
       </div>
       <div className={`hidden h-auto md:flex mx-4 xl:mx-0 justify-between text-4xl`}>
         <Link className={(currentPropertyType === 'townhouse') ? "w-full text-center border-solid border-gray-300 border-t-2 border-r-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/nha_pho${(searchParams?.toString()) ? `?${searchParams?.toString}` : ''}`}>Nhà Phố</Link>
         <Link className={(currentPropertyType === 'villa') ? "w-full text-center border-solid border-gray-300 border-t-2 border-x-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/biet_thu${(searchParams?.toString()) ? `?${searchParams?.toString}` : ''}`}>Biệt Thự</Link>
-        <Link className={(currentPropertyType === 'apartment') ? "w-full text-center border-solid border-gray-300 border-t-2 border-x-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/dat_nen${(searchParams?.toString()) ? `?${searchParams?.toString}` : ''}`}>Đất Nền</Link>
-        <Link className={(currentPropertyType === 'land') ? "w-full text-center border-solid border-gray-300 border-t-2 border-l-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/can_ho${(searchParams?.toString()) ? `?${searchParams?.toString}` : ''}`}>Căn Hộ</Link>
+        <Link className={(currentPropertyType === 'land') ? "w-full text-center border-solid border-gray-300 border-t-2 border-x-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/dat_nen${(searchParams?.toString()) ? `?${searchParams?.toString}` : ''}`}>Đất Nền</Link>
+        <Link className={(currentPropertyType === 'apartment') ? "w-full text-center border-solid border-gray-300 border-t-2 border-l-2 py-2 rounded-t-sm" : "w-full text-gray-400 text-center py-2 border-solid border-gray-300 border-b-2"} href={`/${list_path[1]}/can_ho${(searchParams?.toString()) ? `?${searchParams?.toString}` : ''}`}>Căn Hộ</Link>
       </div>
       
       <div className="md:flex items-center hidden space-y-2 md:space-y-0 py-2 gap-4">
